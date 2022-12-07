93fabdf5464583d530b43151a7ae8aa37d6c6e54
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-12-08 00:40:28 +0530

    movielisting

--- src/components/Movielist/MovieList.jsx
+++ src/components/Movielist/MovieList.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import {useSelector} from "react-redux";
 import { getAllMovies } from '../../features/movies/MovieSlice';
+import MovieCard from '../MovieCard/MovieCard';
+import "./MovieList.scss"
 const MovieList = () => {
   const movies = useSelector(getAllMovies);
+  let renderMovies  = "";
+
+  renderMovies =
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => (
+        <MovieCard key={index} data={movie} />
+      ))
+    ) : (
+      <div className="movies-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+  console.log("hello")
   console.log(movies)
   return (
-    <div>MovieList</div>
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+    </div>
   )
 }
 
